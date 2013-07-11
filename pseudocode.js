02029b202e2d6d564a9c6d18f21bed304477df1e
Dan Tao <daniel.tao@gmail.com>
2013-07-11 10:22:23 -0700

    moved stuff around again, made a proper Pseudocode module

--- pseudocode.js
+++ pseudocode.js
@@ -1,14 +1,52 @@
-var fs   = require('fs');
-var path = require('path');
+var fs      = require('fs');
+var path    = require('path');
+var esprima = require('esprima');
 
-var RubyGenerator = require('./lib/generators/rubyGenerator.js');
+// For support multiple output mechanisms (e.g., console, string)
+var outputs = require('./lib/outputs.js');
 
-// Pick a file, any file.
-var file       = path.join('samples', 'binarySearch.js');
-var js         = fs.readFileSync(file);
-var generator  = new RubyGenerator(js);
-var outputFile = path.basename(file, '.js') + '.rb';
+// "Supported" output languages
+var ruby = require('./lib/ruby.js');
 
-generator.generate(new StreamOutput(fs.createWriteStream(path.join('samples', outputFile), {
-  encoding: 'utf-8'
-})));
+function Pseudocode(ast) {
+  this.ast = ast;
+}
+
+Pseudocode.fromFile = function(filePath) {
+  var lang = inferLanguageFromExtension(filePath);
+  var code = fs.readFileSync(filePath);
+  return Pseudocode['from' + lang](code);
+};
+
+Pseudocode.fromJavaScript = function(js) {
+  var ast = esprima.parse(js);
+  return new Pseudocode(ast);
+};
+
+Pseudocode.prototype.outputRuby = function(output) {
+  ruby.emit(this.ast, output);
+};
+
+Pseudocode.prototype.outputRubyToFile = function(filePath) {
+  var stream = fs.createWriteStream(filePath, { encoding: 'utf-8' });
+  var output = new outputs.Stream(stream);
+  this.outputRuby(output);
+}
+
+Pseudocode.prototype.toRuby = function() {
+  var output = new outputs.String();
+  this.outputRuby(output);
+  return output.toString();
+};
+
+function inferLanguageFromExtension(filePath) {
+  switch (path.extname(filePath)) {
+    case '.js':
+      return 'JavaScript';
+
+    default:
+      throw 'Only JavaScript is supported right now. (And I use that word loosely.)';
+  }
+}
+
+module.exports = Pseudocode;
