ec3d50b3718cda2ae614f6ff15ece947a05c3f82
Dan Tao <daniel.tao@gmail.com>
2013-07-12 07:48:19 -0700

    WIP on major refactor (BIG TIME)

--- pseudocode.js
+++ pseudocode.js
@@ -1,16 +1,13 @@
 var fs      = require('fs');
 var path    = require('path');
 var esprima = require('esprima');
+var Lazy    = require('lazy.js');
 
-// For support multiple output mechanisms (e.g., console, string)
+// For supporting multiple output mechanisms (e.g., console, string)
 var outputs = require('./lib/outputs.js');
 
-// "Supported" output languages
-var csharp = require('./lib/csharp.js');
-var ruby   = require('./lib/ruby.js');
-
 function Pseudocode(ast) {
-  this.ast = ast;
+  this.program = new Pseudocode.Program(ast);
 }
 
 Pseudocode.fromFile = function(filePath) {
@@ -24,38 +21,6 @@ Pseudocode.fromJavaScript = function(js) {
   return new Pseudocode(ast);
 };
 
-Pseudocode.prototype.outputCSharp = function(output) {
-  csharp.emit(this.ast, output);
-};
-
-Pseudocode.prototype.outputCSharpToFile = function(filePath) {
-  var stream = fs.createWriteStream(filePath, { encoding: 'utf-8' });
-  var output = new outputs.Stream(stream);
-  this.outputCSharp(output);
-}
-
-Pseudocode.prototype.toCSharp = function() {
-  var output = new outputs.String();
-  this.outputCSharp(output);
-  return output.toString();
-};
-
-Pseudocode.prototype.outputRuby = function(output) {
-  ruby.emit(this.ast, output);
-};
-
-Pseudocode.prototype.outputRubyToFile = function(filePath) {
-  var stream = fs.createWriteStream(filePath, { encoding: 'utf-8' });
-  var output = new outputs.Stream(stream);
-  this.outputRuby(output);
-}
-
-Pseudocode.prototype.toRuby = function() {
-  var output = new outputs.String();
-  this.outputRuby(output);
-  return output.toString();
-};
-
 function inferLanguageFromExtension(filePath) {
   switch (path.extname(filePath)) {
     case '.js':
@@ -64,6 +29,196 @@ function inferLanguageFromExtension(filePath) {
     default:
       throw 'Only JavaScript as a source language is supported right now. (And I use that word loosely.)';
   }
+}
+
+Pseudocode.Node = function() {};
+
+Pseudocode.Node.inherit = function(functions) {
+  functions = functions || {};
+
+  var ctor = function(rawNode, parent) {
+    var self      = this;
+    self.node     = rawNode;
+    self.parent   = parent;
+    self.depth    = (parent && parent.depth + 1) || 0;
+    self.children = Lazy(self.rawChildren()).map(function(rawNode) {
+      return Pseudocode.Node.wrap(rawNode);
+    }).toArray();
+
+    if (typeof self.init === 'function') {
+      self.init();
+    }
+  };
+
+  ctor.prototype = new Pseudocode.Node();
+
+  Lazy(functions).each(function(f, name) {
+    ctor.prototype[name] = f;
+  });
+
+  return ctor;
+};
+
+Pseudocode.Node.wrap = function(rawNode) {
+  var ctor = Pseudocode[rawNode.type];
+  if (typeof ctor !== 'function') {
+    Pseudocode.NodeException('Unknown node type ' + rawNode.Type, rawNode);
+  }
+  return new ctor(rawNode, this);
+};
+
+Pseudocode.Node.prototype.rawChildren = function() {
+  return [];
+};
+
+Pseudocode.Node.prototype.wrap = function() {
+  var self = this,
+      args = Array.prototype.slice.call(arguments);
+
+  Lazy(args).each(function(prop) {
+    var propertyToWrap = self.node[prop];
+    self[prop] = propertyToWrap instanceof Array ?
+      new Pseudocode.NodeCollection(propertyToWrap) :
+      Pseudocode.Expression.wrap(propertyToWrap);
+  });
+};
+
+Pseudocode.Node.prototype.eachChild = function(fn) {
+  Lazy(this.children).each(fn);
+};
+
+Pseudocode.Node.prototype.toString = function() {
+  return 'Pseudocode.' + this.node.type;
+};
+
+Pseudocode.Node.prototype.output = function(output, content) {
+  output.out(content, this.depth);
+};
+
+Pseudocode.Expression = nodeType();
+
+Pseudocode.Expression.inherit = function(properties) {
+  var ctor = function(rawNode, parent) {
+    var self      = this;
+    self.node     = rawNode;
+    self.parent   = parent;
+
+    Lazy(properties).each(function(prop) {
+      self[prop] = rawNode[prop];
+    });
+  };
+
+  ctor.prototype = new Pseudocode.Expression();
+
+  return ctor;
+};
+
+Pseudocode.Expression.wrap = function(rawNode) {
+  var ctor = Pseudocode[rawNode.type];
+  if (typeof ctor !== 'function') {
+    Pseudocode.NodeException('Unknown expression type: ' + rawNode.type, rawNode);
+  }
+  return new ctor(rawNode, this);
+};
+
+Pseudocode.NodeCollection = function(array) {
+  this.nodes  = Lazy(array).map(Pseudocode.Node.wrap).toArray();
+  this.length = this.nodes.length;
+};
+
+Pseudocode.NodeCollection.prototype.get = function(i) {
+  return this.nodes[i];
+};
+
+Pseudocode.NodeCollection.prototype.each = function(fn) {
+  Lazy(this.nodes).each(fn);
+};
+
+Pseudocode.NodeCollection.prototype.toString = function() {
+  return 'Pseudocode.NodeCollection';
+};
+
+Pseudocode.NodeException = function(message, rawNode) {
+  throw message + ':\n' +
+    'Keys: ' + Object.keys(rawNode) + '\n' +
+    JSON.stringify(rawNode, null, 2).substring(0, 250) + '...';
+};
+
+Pseudocode.Program = nodeType({
+  rawChildren: function() {
+    return this.node.body;
+  }
+});
+
+Pseudocode.FunctionDeclaration = nodeType({
+  init: function() {
+    this.wrap('id', 'params');
+  },
+
+  rawChildren: function() {
+    // body is a block statement
+    return [this.node.body];
+  }
+});
+
+Pseudocode.BlockStatement = nodeType({
+  rawChildren: function() {
+    return this.node.body;
+  }
+});
+
+Pseudocode.VariableDeclaration = nodeType({
+  rawChildren: function() {
+    return this.node.declarations;
+  }
+});
+
+Pseudocode.VariableDeclarator = nodeType({
+  init: function() {
+    this.wrap('id', 'init');
+  }
+});
+
+Pseudocode.ForStatement = nodeType({
+  rawChildren: function() {
+    return this.body;
+  }
+});
+
+Pseudocode.IfStatement = nodeType({
+  rawChildren: function() {
+    var children = [this.node.consequent];
+    if (this.node.alternate) {
+      children.push(this.node.alternate);
+    }
+    return children;
+  }
+});
+
+Pseudocode.WhileStatement = nodeType({
+  rawChildren: function() {
+    // body is a block statement
+    return [this.node.body];
+  }
+});
+
+Pseudocode.ExpressionStatement = nodeType();
+
+Pseudocode.ReturnStatement = nodeType();
+
+Pseudocode.Identifier = exprType(['name']);
+
+Pseudocode.CallExpression = exprType(['callee', 'arguments']);
+
+Pseudocode.MemberExpression = exprType(['object', 'property']);
+
+// convenience methods
+function nodeType(functions) {
+  return Pseudocode.Node.inherit(functions);
+}
+
+function exprType(properties) {
+  return Pseudocode.Expression.inherit(properties);
 }
 
 module.exports = Pseudocode;
--- test.js
+++ test.js
@@ -1,7 +1,8 @@
 var fs = require('fs');
-var js = fs.readFileSync('samples/binarySearch.js', 'utf-8')
+var js = fs.readFileSync('samples/countingSort.js', 'utf-8')
 
 var Pseudocode = require('./pseudocode.js');
+require('./lib/ruby.js');
 
 console.log('JavaScript (original)');
 console.log(js);
@@ -10,6 +11,6 @@ console.log('');
 console.log('Ruby');
 Pseudocode.fromJavaScript(js).outputRuby();
 
-console.log('');
-console.log('C#');
-Pseudocode.fromJavaScript(js).outputCSharp();
+// console.log('');
+// console.log('C#');
+// Pseudocode.fromJavaScript(js).outputCSharp();
