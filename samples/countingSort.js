4d2599bbfb7581dd8130e744d3eb6f363b419ee9
Dan Tao <daniel.tao@gmail.com>
2013-07-11 17:47:01 -0700

    added countingSort example

---
+++ samples/countingSort.js
@@ -0,0 +1,41 @@
+function countingSort(array) {
+  var range  = minMax(array),
+      min    = range[0],
+      counts = createArray(range);
+
+  for (var i = 0; i < array.length; ++i) {
+    counts[array[i] - min] += 1;
+  }
+
+  var result = [];
+  for (var i = 0; i < counts.length; ++i) {
+    while (counts[i] > 0) {
+      result.push(i + min);
+    }
+  }
+
+  return result;
+}
+
+function createArray(range) {
+  var arr = [];
+  for (var i = range[0]; i <= range[1]; ++i) {
+    arr.push(0);
+  }
+  return arr;
+}
+
+function minMax(array) {
+  var min = array[0],
+      max = array[0];
+
+  for (var i = 1; i < array.length; ++i) {
+    if (array[i] < min) {
+      min = array[i];
+    }
+    if (array[i] > max) {
+      max = array[i];
+    }
+  }
+  return [min, max];
+}
